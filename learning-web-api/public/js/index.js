7e6fba43af076a711ac39b4c24da3f297bc5758f
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-07 17:40:28 +0900

    HTMLのフォームからAPIを実行

---
+++ learning-web-api/public/js/index.js
@@ -0,0 +1,10 @@
+const indexModule = (() => {
+    // 検索ボタンをクリックした時のイベントリスナー 設定
+    document.getElementById("search-btn")
+        .addEventListener('click', () => {
+            return searchModule.searchUsers();
+        })
+
+    // UsersモジュールのfetchAllUsersメソッドを呼び出す
+    return usersModule.fetchAllUsers();
+})();
